333ba12bda2e28f129fa918b36138fbb383091db
Martin Petr <martin.petr.276@gmail.com>
2021-04-17 13:09:28 +0200

    chore: add storybook

---
+++ src/Visualizer.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Visualizer from "./Visualizer";
+
+export default {
+  title: "Components/Visualizer",
+  component: Visualizer,
+};
+
+export const Basic = () => <Visualizer />;
