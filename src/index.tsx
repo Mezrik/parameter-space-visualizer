4f7fe675957de1c33b963a291f7f7609b70f6748
Martin Petr <martin.petr.276@gmail.com>
2021-04-17 13:17:30 +0200

    chore: remove index.html and dev build

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Visualizer from "./Visualizer";
-
-ReactDOM.render(<Visualizer />, document.querySelector("#root"));
+export { default as Visualizer } from "./Visualizer";
