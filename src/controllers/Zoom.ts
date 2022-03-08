7972fd81523c06e56070b152f16aa4d2be3cc302
Martin Petr <martin.petr.276@gmail.com>
2022-03-08 18:57:34 +0100

    feat: add canvas (regions) scaling

--- src/controllers/Zoom.ts
+++ src/controllers/Zoom.ts
@@ -1,40 +1,26 @@
 import { zoom, ZoomedElementBaseType, ZoomTransform, ZoomScale } from "d3";
 
-type ZoomListener<XScale, YScale> = (xScale: XScale, yScale?: YScale) => void;
+type ZoomListener = (translate: ZoomTransform) => void;
 
-class Zoom<
-  ZoomRefElement extends ZoomedElementBaseType,
-  XScale extends ZoomScale,
-  YScale extends ZoomScale
-> {
-  private xScale: XScale;
-  private yScale?: YScale;
-
-  private zoomListeners: ZoomListener<XScale, YScale>[] = [];
+class Zoom<ZoomRefElement extends ZoomedElementBaseType> {
+  private zoomListeners: ZoomListener[] = [];
 
   // Zoom behaviour, this should be attached to the zoom base (svg, canvas, etc.)
   public zoom = zoom<ZoomRefElement, unknown>();
 
-  constructor(xScale: XScale, yScale?: YScale, scaleExtent?: [number, number]) {
-    this.xScale = xScale;
-    this.yScale = yScale;
-
+  constructor(scaleExtent?: [number, number]) {
     if (scaleExtent) this.zoom.scaleExtent(scaleExtent);
     this.zoom.on("zoom", this.handleZoom);
   }
 
-  public onChange(listener: ZoomListener<XScale, YScale>) {
+  public onChange(listener: ZoomListener) {
     this.zoomListeners.push(listener);
   }
 
   private handleZoom = (event: any) => {
     const transform: ZoomTransform = event.transform;
 
-    let updatedY: YScale;
-    const updatedX = transform.rescaleX(this.xScale);
-    if (this.yScale) updatedY = transform.rescaleY(this.yScale);
-
-    this.zoomListeners.forEach((cb) => cb(updatedX, updatedY));
+    this.zoomListeners.forEach((cb) => cb(transform));
   };
 }
 
