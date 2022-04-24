22487f89afbff9a1ebb62643a4d9e9e19fb39e19
Martin Petr <martin.petr.276@gmail.com>
2022-04-24 21:59:40 +0200

    feat: add worker loading the csv steam

--- src/lib/data/dataStreamWorker.ts
+++ src/lib/data/dataStreamWorker.ts
@@ -1,7 +1,52 @@
 import { expose } from "comlink";
+import { csvParse } from "d3-dsv";
+import { csvStreamParser } from "./utils";
 
-const streamData = () => {
-  console.log("test");
+type OnLoadCallback = (values: {}[]) => void;
+
+const startStream =
+  (r: Response, parse: (xs: Uint8Array) => {}[], onLoad: OnLoadCallback) =>
+  (controller: ReadableStreamController<Uint8Array>) => {
+    const reader = r.body!.getReader();
+
+    const read = async () => {
+      const { done, value } = await reader.read();
+      if (done || !value) {
+        controller.close();
+        return;
+      }
+
+      const parsed = parse(value);
+      onLoad(parsed);
+
+      controller.enqueue(value);
+      read();
+    };
+
+    read();
+  };
+
+const streamData = async (filename: string, onLoad: OnLoadCallback) => {
+  const response = await fetch(filename);
+
+  if (!response.body) {
+    throw Error("ReadableStream is not supported in your browser.");
+  }
+
+  const parser = csvStreamParser();
+
+  const streamedResponse = new Response(
+    new ReadableStream({
+      start: startStream(response, parser.parseChunk, onLoad),
+    })
+  );
+
+  const data = await streamedResponse.text();
+
+  return csvParse(data);
 };
 
-expose({ streamData });
+const worker = { streamData };
+export type DataStreamWorker = typeof worker;
+
+expose(worker);
