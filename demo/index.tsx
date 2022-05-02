3a281f730d648ff0d7795022154795d2d67f0b6e
Martin Petr <martin.petr.276@gmail.com>
2022-05-02 18:19:40 +0200

    feat: make demo run with preact

---
+++ demo/index.tsx
@@ -0,0 +1,51 @@
+import { render } from "preact";
+import { useState, useEffect } from "preact/hooks";
+import { createProabilityColorScale } from "../src/helpers/general";
+import ProbabilitySamplingChart from "../src/ProbabilitySamplingChart";
+import { RegionResultValue } from "../src/lib/data/parse";
+
+const COLOR_MAPPING: Record<RegionResultValue, string> = {
+  true: "#f0c928",
+  false: "#ab0d0c",
+  unknown: "#fbe6c2",
+  partially_sat: "#ffdc4f",
+  partially_violated: "#fbe6c2",
+  center_sat: "#e0c141",
+  center_violated: "#fbe6c2",
+};
+
+const colorScale = createProabilityColorScale([
+  COLOR_MAPPING.false,
+  COLOR_MAPPING.true,
+]);
+
+const App = () => {
+  const [container, ref] = useState<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (container) {
+      const chart = new ProbabilitySamplingChart(container, {
+        options: {
+          color: ({ value }) => {
+            return typeof value === "number" ? colorScale(value) : "#fde6c4";
+          },
+          margin: { top: 20, right: 30, bottom: 30, left: 40 },
+        },
+        expression: "(-1 * ((p+(-1)) * (q*r+(-1)*r+(-1)*q+1)))/(q*r+(-1)*r+1)",
+        intervals: [
+          // { name: "pL", start: 0, end: 1 },
+          // { name: "pK", start: 0, end: 1 },
+          { name: "p", start: 0, end: 1 },
+          { name: "q", start: 0, end: 0.5 },
+          { name: "r", start: 1 / 10, end: 3 / 10 },
+        ],
+        width: 800,
+        height: 800,
+      });
+    }
+  }, [container]);
+
+  return <div ref={ref}></div>;
+};
+
+render(<App />, document.body);
--- rollup.config.js
+++ rollup.config.js
@@ -5,65 +5,86 @@ import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-import replace from "@rollup/plugin-replace";
 import { visualizer } from "rollup-plugin-visualizer";
 import webWorkerLoader from "rollup-plugin-web-worker-loader";
 
 const isProd = process.env.NODE_ENV === "production";
 const visualizeSpace = process.env.VISUALIZE_SPACE === "true";
 
-const extensions = [".js", ".ts"];
+const prodExtensions = [".js", ".ts"];
+const demoExtensions = [...prodExtensions, ".jsx", ".tsx"];
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "es/index.js",
-      format: "es",
-      plugins: [isProd && terser()],
-    },
-    {
-      file: `umd/index.js`,
-      format: "umd",
-      name: "paramVis",
-      plugins: [isProd && terser()],
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve({
-      extensions,
-      preventAssignment: true,
-    }),
-    commonjs({
-      include: /node_modules/,
-    }),
-    webWorkerLoader(),
-    typescript(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(
-        isProd ? "production" : "development"
-      ),
-      preventAssignment: true,
-    }),
-    babel({
-      extensions,
-      exclude: /node_modules/,
-      babelHelpers: "runtime",
-      presets: ["@babel/preset-env", "@babel/preset-typescript"],
-      plugins: ["@babel/plugin-transform-runtime"],
-    }),
-    visualizeSpace &&
-      visualizer({
-        open: true,
-        template: "treemap",
-      }),
-    !isProd &&
-      serve({
-        open: true,
-        contentBase: ["", "public"],
-        port: 9000,
-      }),
-    !isProd && livereload({ watch: ["src", "umd"], port: 9000 }),
-  ],
-};
+const getCommonPlugins = (extensions, babelPlugins = []) => [
+  resolve({
+    extensions,
+    preventAssignment: true,
+  }),
+  commonjs({
+    include: /node_modules/,
+  }),
+  webWorkerLoader(),
+  typescript(),
+  babel({
+    extensions,
+    exclude: /node_modules/,
+    babelHelpers: "runtime",
+    presets: ["@babel/preset-env", "@babel/preset-typescript"],
+    plugins: ["@babel/plugin-transform-runtime", ...babelPlugins],
+  }),
+];
+
+export default [
+  isProd
+    ? {
+        input: "src/index.ts",
+        output: [
+          {
+            file: "es/index.js",
+            format: "es",
+            plugins: [terser()],
+          },
+          {
+            file: `umd/index.js`,
+            format: "umd",
+            name: "paramVis",
+            plugins: [terser()],
+            sourcemap: true,
+          },
+        ],
+        plugins: [
+          ...getCommonPlugins(prodExtensions),
+          visualizeSpace &&
+            visualizer({
+              open: true,
+              template: "treemap",
+            }),
+        ],
+      }
+    : {
+        input: "demo/index.tsx",
+        output: {
+          file: `demo/dist/index.js`,
+          format: "umd",
+          name: "paramVis",
+          sourcemap: true,
+        },
+
+        plugins: [
+          ...getCommonPlugins(demoExtensions, [
+            [
+              "@babel/plugin-transform-react-jsx",
+              {
+                pragma: "h",
+                pragmaFrag: "Fragment",
+              },
+            ],
+          ]),
+          serve({
+            open: true,
+            contentBase: ["", "demo"],
+            port: 9000,
+          }),
+          livereload({ watch: ["src", "demo"], port: 9000 }),
+        ],
+      },
+];
