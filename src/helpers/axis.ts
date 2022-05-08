6746befce9dd7e1f63c18c11b4ecea9476a13cd2
Martin Petr <martin.petr.276@gmail.com>
2022-05-08 11:30:18 +0200

    feat: add axis labels

--- src/helpers/axis.ts
+++ src/helpers/axis.ts
@@ -1,38 +1,37 @@
-import { axisBottom, axisLeft } from "d3-axis";
-import { BaseType, Selection } from "d3-selection";
-import { theme } from "../constants/styles";
-import { AnyD3Scale } from "../types/scale";
-import { SimpleSelection } from "../types/selection";
+import { axisBottom, axisLeft } from 'd3-axis';
+import { BaseType, Selection } from 'd3-selection';
+import { theme } from '../constants/styles';
+import { AnyD3Scale } from '../types/scale';
+import { SimpleSelection } from '../types/selection';
 
 const styleDomain = (stroke: number) => (g: SimpleSelection<SVGGElement>) =>
-  g
-    .select(".domain")
-    .attr("stroke-width", stroke)
-    .attr("stroke", theme.colors.lightGrey);
+  g.select('.domain').attr('stroke-width', stroke).attr('stroke', theme.colors.lightGrey);
 
 const styleTickLines = (
-  g: SimpleSelection<SVGGElement>
+  g: SimpleSelection<SVGGElement>,
 ): Selection<BaseType, unknown, SVGGElement, unknown> =>
-  g.selectAll(".tick line").attr("stroke", theme.colors.lightGrey);
+  g.selectAll('.tick line').attr('stroke', theme.colors.lightGrey);
 
 const styleTickText = (
-  g: SimpleSelection<SVGGElement>
+  g: SimpleSelection<SVGGElement>,
 ): Selection<BaseType, unknown, SVGGElement, unknown> =>
-  g
-    .selectAll(".tick text")
-    .style("font-size", "0.75rem")
-    .attr("fill", theme.colors.textColor);
+  g.selectAll('.tick text').style('font-size', '0.75rem').attr('fill', theme.colors.textColor);
+
+export const styleAxisLabel = (text: SimpleSelection<SVGTextElement>) =>
+  text
+    .attr('fill', theme.colors.textColor)
+    .style('font-size', '14px')
+    .style('font-weight', 'bold')
+    .attr('text-anchor', 'center');
 
 const styleGridLines =
   (color: string = theme.colors.white) =>
-  (
-    g: SimpleSelection<SVGGElement>
-  ): Selection<BaseType, unknown, SVGGElement, unknown> =>
+  (g: SimpleSelection<SVGGElement>): Selection<BaseType, unknown, SVGGElement, unknown> =>
     g
-      .selectAll(".tick line")
-      .attr("stroke", color)
-      .attr("opacity", 0.3)
-      .attr("stroke-dasharray", "7 3");
+      .selectAll('.tick line')
+      .attr('stroke', color)
+      .attr('opacity', 0.3)
+      .attr('stroke-dasharray', '7 3');
 
 const makeXGridLines = (xScale: AnyD3Scale) => {
   return axisBottom(xScale).ticks(5);
@@ -42,31 +41,28 @@ const makeYGridLines = (yScale: AnyD3Scale) => {
   return axisLeft(yScale).ticks(5);
 };
 
-const removeDomain = (g: SimpleSelection<SVGGElement>) =>
-  g.select(".domain").remove();
+const removeDomain = (g: SimpleSelection<SVGGElement>) => g.select('.domain').remove();
 
 export const xGridLinesFactory =
-  (height: number, xScale: AnyD3Scale, color?: string) =>
-  (g: SimpleSelection<SVGGElement>) => {
-    g.attr("class", "grid")
-      .attr("transform", "translate(0," + height + ")")
+  (height: number, xScale: AnyD3Scale, color?: string) => (g: SimpleSelection<SVGGElement>) => {
+    g.attr('class', 'grid')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(
         makeXGridLines(xScale)
           .tickSize(-height)
-          .tickFormat(() => "")
+          .tickFormat(() => ''),
       )
       .call(removeDomain)
       .call(styleGridLines(color));
   };
 
 export const yGridLinesFactory =
-  (width: number, yScale: AnyD3Scale, color?: string) =>
-  (g: SimpleSelection<SVGGElement>) => {
-    g.attr("class", "grid")
+  (width: number, yScale: AnyD3Scale, color?: string) => (g: SimpleSelection<SVGGElement>) => {
+    g.attr('class', 'grid')
       .call(
         makeYGridLines(yScale)
           .tickSize(-width)
-          .tickFormat(() => "")
+          .tickFormat(() => ''),
       )
       .call(removeDomain)
       .call(styleGridLines(color));
@@ -75,12 +71,12 @@ export const yGridLinesFactory =
 export const xAxisFactory =
   (height: number, xScale: AnyD3Scale, stroke = 2) =>
   (g: SimpleSelection<SVGGElement>) => {
-    g.attr("transform", `translate(0,${height})`)
+    g.attr('transform', `translate(0,${height})`)
       .call(axisBottom(xScale))
       .call(styleDomain(stroke))
       .call(styleTickLines)
       .call(styleTickText)
-      .call((g) => g.selectAll(".tick text").attr("y", 14));
+      .call(g => g.selectAll('.tick text').attr('y', 14));
   };
 
 export const yAxisFactory =
@@ -90,5 +86,5 @@ export const yAxisFactory =
       .call(styleDomain(stroke))
       .call(styleTickLines)
       .call(styleTickText)
-      .call((g) => g.selectAll(".tick text").attr("x", -14));
+      .call(g => g.selectAll('.tick text').attr('x', -14));
   };
