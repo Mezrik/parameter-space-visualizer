97ff22e74b249268217d50a916d341d0943c40f2
Martin Petr <martin.petr.276@gmail.com>
2022-05-08 11:30:18 +0200

    feat: add axis labels

--- src/components/Axes.ts
+++ src/components/Axes.ts
@@ -1,14 +1,19 @@
 import { zoomIdentity, ZoomTransform } from 'd3-zoom';
 import Config from '../Config';
+import { AXIS_LABEL_OFFSET } from '../constants/common';
+import { theme } from '../constants/styles';
 import DataController from '../controllers/DataController';
 
-import { xAxisFactory, yAxisFactory } from '../helpers/axis';
+import { styleAxisLabel, xAxisFactory, yAxisFactory } from '../helpers/axis';
 import { AnyD3Scale, DataControllerScaleTuple } from '../types/scale';
 import { SimpleSelection } from '../types/selection';
 
 class Axes {
   private gx?: SimpleSelection<SVGGElement>;
   private gy?: SimpleSelection<SVGGElement>;
+
+  private labelX?: SimpleSelection<SVGTextElement>;
+  private labelY?: SimpleSelection<SVGTextElement>;
 
   private config: Config<any>;
   private dataController: DataController<any>;
@@ -32,6 +37,8 @@ class Axes {
 
     // Y scale is not guaranteed, since the chart supports 1D chart
     if (yScale) this.gy.call(yAxisFactory(yScale.scale));
+
+    this.initLabels();
   }
 
   public redrawAxes = (transform: ZoomTransform = zoomIdentity) => {
@@ -46,6 +53,29 @@ class Axes {
     this.gx?.call(xAxisFactory(yMax, transform.rescaleX(xScale.scale) as AnyD3Scale));
 
     if (yScale) this.gy?.call(yAxisFactory(transform.rescaleY(yScale.scale) as AnyD3Scale));
+
+    if (!this.labelX || !this.labelY) this.initLabels();
+    this.labelX?.text(this.config.params?.[0] ?? '');
+    this.labelY?.text(this.config.params?.[1] ?? '');
+  };
+
+  private initLabels = () => {
+    this.labelX = this.gx
+      ?.append('text')
+      .attr('x', this.config.xMax / 2)
+      .attr('y', AXIS_LABEL_OFFSET)
+      .attr('dy', '0.75rem')
+      .text(this.config.params?.[0] ?? '')
+      .call(styleAxisLabel);
+
+    this.labelY = this.gy
+      ?.append('text')
+      .attr('x', -AXIS_LABEL_OFFSET)
+      .attr('y', this.config.yMax / 2)
+      .attr('dx', '-0.75rem')
+      .attr('rotate', '-90')
+      .text(this.config.params?.[1] ?? '')
+      .call(styleAxisLabel);
   };
 }
 
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -6,7 +6,7 @@ export const UNDEFINED_CHART_VALUE = 0;
 
 export const ZERO_MARGIN: Margin = { top: 0, left: 0, right: 0, bottom: 0 };
 
-export const DEFAULT_CHART_MARGIN: Margin = { top: 20, right: 30, bottom: 30, left: 40 };
+export const DEFAULT_CHART_MARGIN: Margin = { top: 20, right: 30, bottom: 60, left: 60 };
 
 export const COLOR_MAPPING: Record<RegionResultValue, string> = {
   true: '#f0c928',
@@ -19,3 +19,5 @@ export const COLOR_MAPPING: Record<RegionResultValue, string> = {
 };
 
 export const DEFAUL_COLOR_SCALE = createProabilityColorScale(['#FF0000', '#FFFF00', '#008000']);
+
+export const AXIS_LABEL_OFFSET = 30;
