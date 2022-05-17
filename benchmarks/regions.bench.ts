35a0047e357c12e30ac0982908a062f664c48e32
Martin Petr <martin.petr.276@gmail.com>
2022-05-17 12:17:01 +0200

    chore: change benchmarking timeout

--- benchmarks/regions.bench.ts
+++ benchmarks/regions.bench.ts
@@ -4,7 +4,7 @@ import { csvToRegionResultsList, RawCSVObject } from '../src/lib/data/parse';
 import { COLOR_MAPPING } from '../src/constants/common';
 
 // @ts-ignore
-import BRP01 from '../data/csv/regions/ok-results/brp01.csv';
+import test01 from '../data/csv/regions/ok-results/test01.csv';
 // @ts-ignore
 import BRP02 from '../data/csv/regions/ok-results/brp02.csv';
 // @ts-ignore
@@ -13,11 +13,11 @@ import BRP03 from '../data/csv/regions/ok-results/brp03.csv';
 const paramVis = require('../umd');
 
 benchmarkSuite('Regions chart', {
-  ['Bench regions chart init with BRP01.csv']: () => {
-    createRegionsChart(BRP01);
+  ['Bench regions chart init with test01.csv']: () => {
+    createRegionsChart(test01);
   },
-  ['Bench regions chart rendering with BRP01.csv']: () => {
-    chartBRP01.redraw();
+  ['Bench regions chart rendering with test01.csv']: () => {
+    chartTest01.redraw();
   },
   ['Bench regions chart init with BRP02.csv']: () => {
     createRegionsChart(BRP02);
@@ -42,6 +42,6 @@ const createRegionsChart = (csv: RawCSVObject) => {
   });
 };
 
-const chartBRP01: CustomRegionsChart<any> = createRegionsChart(BRP01);
+const chartTest01: CustomRegionsChart<any> = createRegionsChart(test01);
 const chartBRP02: CustomRegionsChart<any> = createRegionsChart(BRP02);
 const chartBRP03: CustomRegionsChart<any> = createRegionsChart(BRP03);
