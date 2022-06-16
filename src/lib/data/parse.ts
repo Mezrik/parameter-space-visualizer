30343b103152ea3ea67965732a5741371aadb6e6
Martin Petr <martin.petr.276@gmail.com>
2022-06-16 14:52:36 +0200

    fix: parsing of empty csv lines (shifted space)

--- src/lib/data/parse.ts
+++ src/lib/data/parse.ts
@@ -1,4 +1,4 @@
-import { csvParse } from 'd3-dsv';
+import { csvParse, DSVRowString, autoType } from 'd3-dsv';
 import { ScatterDatum } from '../../types/general';
 
 export type RegionResultValue =
@@ -51,54 +51,77 @@ export const parseValue = (value: string | undefined): RegionResultValue => {
   }
 };
 
+const blankChecker = (row: DSVRowString<string>) => {
+  const blankRow = Object.keys(row).every(k => !row[k]);
+  if (!blankRow) return row;
+};
+
+export const rowToRegionResult =
+  <Value = RegionResultValue>(parseVal?: (v?: string) => Value) =>
+  (rawRow: DSVRowString<string>): RegionResult<Value> | undefined => {
+    const row = blankChecker(rawRow);
+    if (!row) return;
+
+    const value = (parseVal ?? parseValue)(row['value']) as Value;
+    return {
+      value,
+      params: Object.keys(row).reduce<RegionResult<RegionResultValue>['params']>(
+        (params, param) =>
+          param !== 'value' ? { ...params, [param]: parseParam(row[param]) } : params,
+        {},
+      ),
+    };
+  };
+
 export const csvToRegionResultsList = <Value = RegionResultValue>(
   raw: RawCSVObject,
   parseVal?: (v?: string) => Value,
 ): RegionResults<Value> => {
-  return raw.map(result => {
-    const value = (parseVal ?? parseValue)(result['value']) as Value;
-    return {
-      value,
-      params: Object.keys(result).reduce<RegionResult<RegionResultValue>['params']>(
-        (params, param) =>
-          param !== 'value' ? { ...params, [param]: parseParam(result[param]) } : params,
-        {},
-      ),
-    };
-  });
+  const pFn = rowToRegionResult(parseVal);
+  return raw.reduce<RegionResults<Value>>((acc, row) => {
+    const p = pFn(row);
+    if (p) acc.push(p);
+    return acc;
+  }, []);
 };
 
 export const csvToRegionResults = <Value = RegionResultValue>(
   csv: string,
   parseVal?: (v?: string) => Value,
 ): RegionResults<Value> => {
-  const raw = csvParse(csv);
-
-  return csvToRegionResultsList(raw, parseVal);
+  return csvParse(csv, rowToRegionResult(parseVal));
 };
+
+export const rowToScatterPoint =
+  <Value = RegionResultValue>(parseVal: (v: string) => Value) =>
+  (rawRow: DSVRowString<string>): ScatterDatum<Value> | undefined => {
+    const row = blankChecker(rawRow);
+    if (!row) return;
+
+    return {
+      value: parseVal(row['value']?.trim()!),
+
+      params: Object.keys(row).reduce<ScatterDatum<Value>['params']>((params, param) => {
+        return param !== 'value' && row[param]
+          ? { ...params, [param.trim()]: parseFraction(row[param]!) }
+          : params;
+      }, {}),
+    };
+  };
 
 export const csvToScatterPointsList = <Value>(
   raw: RawCSVObject,
   parseVal: (v: string) => Value,
 ): ScatterDatum<Value>[] => {
-  return raw.map(result => {
-    return {
-      value: parseVal(result['value']?.trim()!),
-
-      params: Object.keys(result).reduce<ScatterDatum<Value>['params']>((params, param) => {
-        return param !== 'value' && result[param]
-          ? { ...params, [param.trim()]: parseFraction(result[param]!) }
-          : params;
-      }, {}),
-    };
-  });
+  const pFn = rowToScatterPoint(parseVal);
+  return raw.reduce<ScatterDatum<Value>[]>((acc, row) => {
+    const p = pFn(row);
+    if (p) acc.push(p);
+    return acc;
+  }, []);
 };
 
 export const csvToScatterPoints = <Value>(
   csv: string,
   parseVal: (v: string) => Value,
-): ScatterDatum<Value>[] => {
-  const raw = csvParse(csv);
-
-  return csvToScatterPointsList(raw, parseVal);
-};
+): ScatterDatum<Value>[] => csvParse(csv, rowToScatterPoint(parseVal));
